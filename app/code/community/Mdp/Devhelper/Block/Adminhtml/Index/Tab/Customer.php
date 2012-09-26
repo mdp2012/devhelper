22f63326213a75dcbf0ed3a51c2eadd305cb9c5d
thebod <thebod@thebod.de>
2012-09-26 19:44:18 +0200

    basic customer form

--- app/code/community/Mdp/Devhelper/Block/Adminhtml/Index/Tab/Customer.php
+++ app/code/community/Mdp/Devhelper/Block/Adminhtml/Index/Tab/Customer.php
@@ -1,7 +1,14 @@
 <?php
 
-class Mdp_Devhelper_Block_Adminhtml_Index_Tab_Customer extends Mage_Adminhtml_Block_Widget_Form
+class Mdp_Devhelper_Block_Adminhtml_Index_Tab_Customer extends Mage_Adminhtml_Block_Widget_Container
 {
+    public function _beforeToHtml()
+    {
+        $this->addBlock();
+
+        return parent::_beforeToHtml();
+    }
+
     public function initForm()
     {
         $form = new Varien_Data_Form();
@@ -9,15 +16,33 @@ class Mdp_Devhelper_Block_Adminhtml_Index_Tab_Customer extends Mage_Adminhtml_Bl
         $fieldset = $form->addFieldset(
             'content_fieldset',
             array(
-                'legend' => Mage::helper('mdp_devhelper')->__('Content'),
+                'legend' => Mage::helper('mdp_devhelper')->__('Customer'),
                 'class'=>'fieldset-wide'
             )
         );
 
-        $fieldset->addField('content_heading', 'text', array(
-            'name'      => 'content_heading',
+        $fieldset->addField('customer_id', 'text', array(
+            'name'      => 'customer_id',
+            'label'     => Mage::helper('mdp_devhelper')->__('Customer ID'),
+            'title'     => Mage::helper('mdp_devhelper')->__('Customer ID'),
+        ));
+
+        $fieldset->addField('mail_template', 'select', array(
+            'name'      => 'mail_template',
+            'label'     => Mage::helper('mdp_devhelper')->__('Email Template'),
+            'title'     => Mage::helper('mdp_devhelper')->__('Email Template'),
+            'values'    => Mage::helper('mdp_devhelper')->getEmailTemplates('customer'),
+        ));
+
+        $fieldset->addField('receiver_email', 'text', array(
+            'name'      => 'receiver_email',
             'label'     => Mage::helper('mdp_devhelper')->__('Receiver Email'),
             'title'     => Mage::helper('mdp_devhelper')->__('Receiver Email'),
+        ));
+
+        $fieldset->addField('send_mail', 'button', array(
+            'name'      => 'send_mail',
+            'value'     => Mage::helper('mdp_devhelper')->__('Send Email'),
         ));
         
         $this->setForm($form);
--- app/code/community/Mdp/Devhelper/Helper/Data.php
+++ app/code/community/Mdp/Devhelper/Helper/Data.php
@@ -2,4 +2,11 @@
 
 class Mdp_Devhelper_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function getEmailTemplates($type = null)
+    {
+        $templates = Mage_Core_Model_Email_Template::getDefaultTemplatesAsOptionsArray();
+        $customTemplates = Mage::getResourceModel('core/email_template_collection')->load()->toOptionArray();
+
+        return array_merge($customTemplates, $templates);
+    }
 }
